8ce228f1ed490c02cffc252f4015dddc06f257d6
Cosmin765 <cosminturtureanu1234@gmail.com>
2024-04-24 23:36:47 +0300

    Convert some services to azure

--- src/app.js
+++ src/app.js
@@ -9,23 +9,23 @@ const os = require('os');
 const {PubSub} = require('@google-cloud/pubsub');
 
 const HOSTNAME = os.hostname();
+const { WebPubSubServiceClient } = require("@azure/web-pubsub");
 
-const { Storage } = require('@google-cloud/storage');
-const bucketName = "documents_database"
 const { PrismaClient, Prisma } = require('@prisma/client')
 const verifyToken = require('./middleware/authMiddleware');
 const { jwtDecode } = require('jwt-decode');
 const { env } = require('process');
+const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
 const prisma = new PrismaClient();
-const storage = new Storage();
+const pubSubServiceClient = new WebPubSubServiceClient("https://doccraft-pubsub.webpubsub.azure.com", env.PUB_SUB_KEY, "permissions_hub");
 
-async function uploadFromMemory(bucketName, destFileName, contents) {
-  await storage.bucket(bucketName).file(destFileName).save(contents);
+// async function uploadFromMemory(bucketName, destFileName, contents) {
+//   await storage.bucket(bucketName).file(destFileName).save(contents);
 
-  console.log(
-    `${destFileName} with contents ${contents} uploaded to ${bucketName}.`
-  );
-}
+//   console.log(
+//     `${destFileName} with contents ${contents} uploaded to ${bucketName}.`
+//   );
+// }
 
 const upload = multer({});
 
@@ -48,18 +48,18 @@ app.get('/', (req, res) => {
   res.status(200).send('Salutare, lume!').end();
 });
 
-async function fetchFile(fileName) {
-  const options = {
-    destination: destFileName,
-  };
+// async function fetchFile(fileName) {
+//   const options = {
+//     destination: destFileName,
+//   };
 
-  const bucket = storage.bucket(bucketName)
-  await bucket.file(fileName).download(options);
+//   const bucket = storage.bucket(bucketName)
+//   await bucket.file(fileName).download(options);
 
-  console.log(
-    `gs://${bucketName}/${fileName} contents sent to frontend.`
-  );
-}
+//   console.log(
+//     `gs://${bucketName}/${fileName} contents sent to frontend.`
+//   );
+// }
 
 
 app.post('/login', async (req, res) => {
@@ -75,39 +75,47 @@ app.post('/login', async (req, res) => {
 });
 
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  verifyToken(req, res, async (req, res) => {
-    const bucketName = 'documents_database';
-    const destFileName = req.file.originalname;
-    const contents = req.file.buffer;
-    await prisma.files.create({
-      data: {
-        file_name: req.file.originalname,
-        file_size: req.file.size
-      }
-    });
-    uploadFromMemory(bucketName, destFileName, contents).catch(console.error);
-    res.status(200).json("ok");
-  })
-});
+// TODO: de vazut aici
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   verifyToken(req, res, async (req, res) => {
+//     const contents = req.file.buffer;
+//     await prisma.files.create({
+//       data: {
+//         file_name: req.file.originalname,
+//         file_size: req.file.size
+//       }
+//     });
+
+//     const blobClient = containerClient.getBlockBlobClient(filename);
+//     await blobClient.uploadData(Buffer.from(content));
+//     res.status(200).json("ok");
+//   })
+// });
+
+const accountName = "doccraftfiles"
+const blobServiceClient = new BlobServiceClient(
+  `https://${accountName}.blob.core.windows.net`,
+  new StorageSharedKeyCredential(accountName, env.BLOB_ACCESS_KEY)
+);
+
+const containerName = "files";
 
 app.get('/files', async (req, res) => {
   try {
-    const { email } = jwtDecode(req.cookies.authToken);
+    // TODO: auth
+    const containerClient = blobServiceClient.getContainerClient(containerName);
 
-    const user = await prisma.users.findFirst({where: {email}});
+    const files = [];
+    for await (const file of containerClient.listBlobsFlat()) {
+      files.push(file.name);
+    }
 
-    const files = (await prisma.file_permissions.findMany({
-      include: {
-        files: true
-      },
-      where: {user_id: user.user_id, permission_type: 'READ'}
-    })).map(permission => permission.files.file_name);
-
-    res.status(200).json(files);
-  } catch (error) {
-    console.error('Error fetching files:', error);
-    res.status(500).send('Server error');
+    res.json(files);
+    return;
+  } catch (err) {
+    console.error('Error fetching files:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+    return;
   }
 });
 
@@ -117,7 +125,9 @@ app.get('/contents/:filename', async (req, res) => {
   if (!filename) {
     return res.status(400).send('Filename is required.');
   }
-  const { email } = jwtDecode(req.cookies.authToken);
+  // TODO: auth
+  // const { email } = jwtDecode(req.cookies.authToken);
+  const email = 'default@default.default';
   const owner = await prisma.users.findFirst({ where: { email: email } });
 
   // TODO: verifica permisiunea de write
@@ -133,17 +143,19 @@ app.get('/contents/:filename', async (req, res) => {
     return res.status(403).send("Permission denied!");
   }
   
-  const file = storage.bucket(bucketName).file(filename);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+
+  const file = containerClient.getBlobClient(filename);
+
+  if (!await file.exists()) {
+    return res.status(404).send('Not Found');
+  }
 
   try {
-    const exists = await file.exists();
-    if (exists[0]) {
-      const contents = await file.download();
-      res.status(200).send(contents);
-    } else {
-      res.status(404).send(`File ${filename} not found.`);
-    }
-  } catch (err) {
+    const buffer = await file.downloadToBuffer();
+
+    return res.status(200).end(buffer);
+  } catch(err) {
     console.error('Error:', err);
     res.status(500).send('Internal Server Error');
   }
@@ -153,8 +165,11 @@ app.post('/save/:filename', express.json(), async (req, res) => {
   const { filename } = req.params;
   const { content } = req.body;
 
-  const { email } = jwtDecode(req.cookies.authToken);
+  // TODO: auth
+  // const { email } = jwtDecode(req.cookies.authToken);
+  const email = 'default@default.default'
 
+  const containerClient = blobServiceClient.getContainerClient(containerName);
 
   if (!email) {
     return res.status(400).send('Email is required.');
@@ -163,6 +178,7 @@ app.post('/save/:filename', express.json(), async (req, res) => {
   if (!filename) {
     return res.status(400).send('Filename is required.');
   }
+
   try {
     const owner = await prisma.users.findFirst({ where: { email: email } });
 
@@ -180,7 +196,8 @@ app.post('/save/:filename', express.json(), async (req, res) => {
       }
     }
 
-    await uploadFromMemory(bucketName, filename, content);
+    const blobClient = containerClient.getBlockBlobClient(filename);
+    await blobClient.uploadData(Buffer.from(content));
     res.status(200).send('File saved successfully.');
   } catch (error) {
     console.error('Error saving file:', error);
@@ -220,7 +237,9 @@ app.post('/share', async (req, res) => {
     try {
       await prisma.file_permissions.create({data: {file_id: file.file_id, user_id: userTo.user_id, permission_type: permissionType}});
       // pubsubTopic.publishJSON({emailFrom: email, emailTo: emailTo, permissionType: permissionType, fileName: filename})
-      pubsubTopic.publishMessage({data: Buffer.from(JSON.stringify({emailFrom: email, emailTo: emailTo, permissionType: permissionType, fileName: filename}))})
+      // pubsubTopic.publishMessage({data: Buffer.from(JSON.stringify({emailFrom: email, emailTo: emailTo, permissionType: permissionType, fileName: filename}))})
+      //TODO: test pub sub service
+      pubSubServiceClient.sendToAll({data: {file_id: file.file_id, user_id: userTo.user_id, permission_type: permissionType}});
     } catch(_) {}
 
     return res.status(200).send('Ok');
